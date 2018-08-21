f352ba1e0a400b01f96f25bf09a9e0be1fcd0de0
Laura Ambrose <laura@kintohub.com>
2018-08-21 18:38:46 +0800

    updated url to refer to staging

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
+// require('./auth/slackAuth.js') // WIP just pseudo code for the slack auth process
 const express = require('express')
 const request = require('request-promise-native')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
+
+// env variables that will become custom params
 const PORT = process.env.PORT || '8000'
 const KINTOHUB_CLIENTID = process.env.KINTOHUB_CLIENTID
 const KINTOHUB_MICROSERVICE = process.env.KINTOHUB_MICROSERVICE
-const databaseMicroserviceUrl = `https://public.api.kintohub.com/${KINTOHUB_CLIENTID}/${KINTOHUB_MICROSERVICE}`
+
+// kintocrud url created using client id and microservice name
+const databaseMicroserviceUrl = `https://public.api.staging.kintohub.com/${KINTOHUB_CLIENTID}/${KINTOHUB_MICROSERVICE}`
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -31,14 +36,15 @@ app.post('/new/player', (req, res) => {
 
   const options = {
     method: 'POST',
-    uri: `${databaseMicroserviceUrl}/players/create`,
+    uri: `${databaseMicroserviceUrl}/create`,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     data: {
       name: name,
       score: 0
-    }
+    },
+    json: true
   }
   request(options)
     .then(() => {
